16c5297ae71d3192629d58cbb52b8971717a5c64
aubert-co <blacket021@gmail.com>
2026-05-02 23:37:31 -0300

    feat(cart): add unauthenticated user cart navigation tests

---
+++ app/cypress/e2e/cart-cycle/cart.unauthenticated.cy.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+
+
+describe('Unauthenticated user navigation (product → profile/cart)',()=>{
+    beforeEach(()=>{
+        cy.visit("/")
+    })
+    it("should return an error message when adding a product to the cart while the user is not logged in",()=>{
+        cy.intercept("GET","http://localhost:8080/api/product?page=1").as("getProducts")
+        cy.wait("@getProducts")
+        cy.get('.product')
+        .first().click()
+
+        cy.contains('Adicionar ao carrinho')
+        .click()
+
+        cy.get(".message_error")
+        .should("contain.text","Faça login para adicionar ao carrinho")
+
+        cy.get("nav i").first()
+        .click()
+
+       
+
+        cy.contains("Você não está logado. Faça login.")
+    })
+})
+
+
+
+
