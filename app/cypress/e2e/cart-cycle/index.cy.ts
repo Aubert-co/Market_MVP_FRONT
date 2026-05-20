682dcce9764c672c5b45f8553910d76ee50193d7
aubert-co <blacket021@gmail.com>
2026-05-19 23:51:24 -0300

    refactor(e2e): refactor cart cycle test

--- app/cypress/e2e/cart-cycle/index.cy.ts
+++ app/cypress/e2e/cart-cycle/index.cy.ts
@@ -1,21 +1,47 @@
-import { userCartMocks } from '../../fixtures/userCart'
-describe("cicle productPage to profile/cart",()=>{
-   
-    it("should enter in the product page add to cart an see this in the cart",()=>{
-        
-        cy.intercept('GET','/user/cart',{
-            statusCode:200,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body:{
-                message:'success',
-                datas:userCartMocks
-            } 
-        }).as('user/cart')
+/// <reference types="cypress" />
 
-        cy.visit('/perfil/carrinho')
-        cy.wait('@user/cart')       
+import {users} from "../../fixtures/user"
+const [user] = users
+describe("cicle productPage to profile/cart",()=>{
+    beforeEach(()=>{
+        cy.login(user)
+        cy.visit("/")
+        
+        
+    })
+    it("should enter in the product page add to cart an see this in the cart and realizes increase ,decrease ,and remove from cart",()=>{
+        cy.intercept("GET","http://localhost:8080/api/product?page=1").as("getProducts")
+        cy.wait("@getProducts")
+        cy.get('.product')
+        .first().click()
+
+        cy.contains('Adicionar ao carrinho')
+        .click()
+
+        cy.get(".message_success")
+        .should("contain.text","Adicionado ao carrinho com sucesso")
+
+        cy.get("nav i").first()
+        .click()
+
+        cy.contains("Meu carrinho")
+        cy.get('[aria-label="Quantidade de itens no carrinho"]')
+            .should('contain', '1')
+        
+        cy.get('[aria-label="Aumentar quantidade"]')
+        .click()
+        cy.get('[aria-label="Quantidade de itens no carrinho"]')
+            .should('contain', '2')
+
+        cy.get('[aria-label="Diminuir quantitdade"]')
+        .click()
+        cy.get('[aria-label="Quantidade de itens no carrinho"]')
+            .should('contain', '1')
+
+        cy.get('[aria-label="Remover item do carrinho"]')
+        .click()
+
+        cy.contains("Seu carrinho está vazio. Adicionar produtos")
     })
 })
 
