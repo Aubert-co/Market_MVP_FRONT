bb95326f545fa9a26739bf98f45187abfa18ecc7
aubert-co <blacket021@gmail.com>
2026-05-19 23:52:44 -0300

    refactor(e2e): refactor login test

--- app/cypress/e2e/login-register/login.cy.ts
+++ app/cypress/e2e/login-register/login.cy.ts
@@ -1,21 +1,17 @@
-const loginApi = "/login"
+import { users } from "cypress/fixtures/user";
+
+ const [user] = users
 describe("Page Login",()=>{
     beforeEach(()=>{
+        cy.reset_mocks()
         cy.visit('/login')
         
     })
     
     it("should log a new user successfully and redirect to /",()=>{
-        cy.intercept('POST',loginApi,{
-            statusCode:201,
-            body:{
-                message:'Sucess'
-            }
-        }).as('loginUserSucess')
-      
-
-        cy.get('input[placeholder="Ex: joao@gmail.com"]').type('user@example.com');
-        cy.get('input[placeholder="Digite uma senha forte!"]').type('1234567');
+       
+        cy.get('input[placeholder="Ex: joao@gmail.com"]').type(user.email);
+        cy.get('input[placeholder="Digite uma senha forte!"]').type(user.password);
        
 
         cy.contains('button','Enviar').click()
@@ -25,64 +21,30 @@ describe("Page Login",()=>{
 
         cy.location('pathname').should('eq', '/');
     })
-    it("should display 'Usuário ou senha inválidos' when status is 400 and stay on the same page",()=>{
-        cy.intercept('POST',loginApi,{
-            statusCode:400,
-            body:{
-                message:''
-            }
-        }).as('loginUser')
-        cy.get('input[placeholder="Ex: joao@gmail.com"]').type('user@example.com');
-        cy.get('input[placeholder="Digite uma senha forte!"]').type('1234567');
+    it("should display 'Usuário ou senha inválidos' when the user password is invalid",()=>{
+        
+        cy.get('input[placeholder="Ex: joao@gmail.com"]').type(user.email);
+        cy.get('input[placeholder="Digite uma senha forte!"]').type(user.password+"e");
        
 
         cy.contains('button','Enviar').click()
         cy.contains('div', 'Usuário ou senha inválidos');
         cy.location('pathname').should('eq', '/login');
     })
-    it("should not redirect to '/' when the status is 500 and should show the message 'Erro interno, tente novamente'",()=>{
-        cy.intercept('POST',loginApi,{
-            statusCode:500,
-            body:{
-                message:'n'
+    it("should return 'Erro interno, tente novamente' when the backend returns status 500",()=>{
+        cy.intercept("POST", "http://localhost:8080/api/login", {
+            statusCode: 500,
+            body: {
+                message: "Internal Server Error"
             }
-        }).as('loginUser')
-        cy.get('input[placeholder="Ex: joao@gmail.com"]').type('user@example.com');
-        cy.get('input[placeholder="Digite uma senha forte!"]').type('1234567');
+        }).as("loginError")
+    
+         cy.get('input[placeholder="Ex: joao@gmail.com"]').type(user.email);
+        cy.get('input[placeholder="Digite uma senha forte!"]').type(user.password+"e");
        
 
         cy.contains('button','Enviar').click()
-        cy.contains('div','Erro interno, tente novamente');
-             cy.location('pathname').should('eq', '/login');
-    })
-    it("should not redirect to '/' when the status is 499 and show the message 'Usuário ou senha inválidos'",()=>{
-        cy.intercept('POST',loginApi,{
-            statusCode:410,
-            body:{
-                message:'n'
-            }
-        }).as('loginUser')
-        cy.get('input[placeholder="Ex: joao@gmail.com"]').type('user@example.com');
-        cy.get('input[placeholder="Digite uma senha forte!"]').type('1234567');
-       
-
-        cy.contains('button','Enviar').click()
-        cy.contains('div','Usuário ou senha inválidos');
-        cy.location('pathname').should('eq', '/login');
-    })
-    it("should not redirect to '/' when the status is 399 and show the message 'Usuário ou senha inválidos'",()=>{
-        cy.intercept('POST',loginApi,{
-            statusCode:399,
-            body:{
-                message:'n'
-            }
-        }).as('loginUser')
-        cy.get('input[placeholder="Ex: joao@gmail.com"]').type('user@example.com');
-        cy.get('input[placeholder="Digite uma senha forte!"]').type('1234567');
-       
-
-        cy.contains('button','Enviar').click()
-        cy.contains('div','Erro interno, tente novamente');
+        cy.contains('div', 'Erro interno, tente novamente');
         cy.location('pathname').should('eq', '/login');
     })
 })
