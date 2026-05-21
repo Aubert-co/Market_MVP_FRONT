bceab24bba38b02ce94987c9d5eec6b666f1b4e0
aubert-co <blacket021@gmail.com>
2026-05-20 23:24:30 -0300

    feat(constants): add new constants

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -3,3 +3,43 @@ export const FIVE_MINUTES = 5 * 60 * 1000;
 export const KEY_STORE = "store-key"
 export const CHECKOUT_KEY = "checkout-key"
 
+
+export const PROMO_ARRAY = [
+  {
+    search: "category=Roupas",
+    title: "Roupas",
+    content: "Confira peças disponíveis na loja"
+  },
+  {
+    search: "category=Esporte",
+    title: "Esporte",
+    content: "Produtos para atividades físicas"
+  },
+  {
+    search: "category=Livros",
+    title: "Livros",
+    content: "Explore títulos disponíveis"
+  },
+  {
+    search: "category=Eletrônicos",
+    title: "Eletrônicos",
+    content: "Gadgets e acessórios modernos"
+  },
+  {
+    search: "category=Cozinha",
+    title: "Cozinha",
+    content: "Utensílios para o dia a dia"
+  },
+  {
+    search: "category=Informática",
+    title: "Informática",
+    content: "Equipamentos e tecnologia"
+  },
+   {
+    search: "maxPrice=50",
+    title: "Até R$50",
+    content: "Produtos com preço acessível"
+  }
+]
+
+
