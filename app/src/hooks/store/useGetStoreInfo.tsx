a47629539dc07e686a5bf2c7d8c50a0e2c7ebe69
aubert-co <blacket021@gmail.com>
2026-05-20 23:26:58 -0300

    refactor(store): improve data handling in useGetStoreInfo hook

--- app/src/hooks/store/useGetStoreInfo.tsx
+++ app/src/hooks/store/useGetStoreInfo.tsx
@@ -1,20 +1,20 @@
 import { usableFetch } from "@/services/fetchs"
-import { DATAS_STORE, serviceGetStores } from "@/services/store/store.services"
+import {  serviceGetStores } from "@/services/store/store.services"
 import type { Store } from "@/types/store.types"
 import { useEffect, useState } from "react"
 
 type State = {
-    datas:Store,
+    datas:Store[],
     status:number,
     message:string
 }
 
 export const useGetStoreInfo = ()=>{
     const [storeInfo,setStoreInfo] = useState<State>({
-        datas:DATAS_STORE,status:0,message:''
+        datas:[],status:0,message:''
     })
     useEffect(()=>{
-        usableFetch<Store,unknown>({
+        usableFetch<Store[],unknown>({
             service:serviceGetStores,
             setDatas:setStoreInfo,
             body:{}
