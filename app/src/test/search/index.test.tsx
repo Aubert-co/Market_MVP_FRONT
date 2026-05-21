3f310800ea06d43a47590a30d522f3986e65012f
aubert-co <blacket021@gmail.com>
2026-05-20 23:53:20 -0300

    test(search): update tests to handle useEffect state updates with act

--- app/src/test/search/index.test.tsx
+++ app/src/test/search/index.test.tsx
@@ -1,44 +1,64 @@
 import { Search } from "@/pages/search"
-import { fireEvent, render } from "@testing-library/react"
+import { fireEvent, render, waitFor,screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import * as searchProduct from "@/services/productsService"
 
 const spy= jest.spyOn(searchProduct,'searchProduct')
 describe("Page search",()=>{
-    it("should successfully send the correct values",()=>{
-        const {getByText,getByTestId,getByPlaceholderText} = render(
+    it("should successfully send the correct values",async()=>{
+        const {getByText} = render(
             <BrowserRouter>
                 <Search/>
             </BrowserRouter>
         )
+       await waitFor(() => {
+  expect(getByText("Filtrar")).toBeInTheDocument()
+})
 
-        expect(getByText("Filtrar")).toBeInTheDocument()
+    fireEvent.click(getByText("Filtrar"))
 
-        fireEvent.click(getByText("Filtrar"))
-        const minPrice = getByTestId("min-price")
-        const maxPrice = getByTestId("max-price")
-        const btnSubmit = getByText("BUSCAR")
-        const inputName = getByPlaceholderText("FAÇA UMA BUSCA")
-        fireEvent.change(minPrice,{target:{
-            value:'50'
-        }})
-         fireEvent.change(maxPrice,{target:{
-            value:'100'
-        }})
-        fireEvent.change(inputName,{
-            target:{value:"camisa polo"}
-        })
-        expect(minPrice).toHaveValue(50)
-        expect(maxPrice).toHaveValue(100)
+    const minPrice = await screen.findByTestId("min-price")
+    const maxPrice = await screen.findByTestId("max-price")
 
-        fireEvent.click(btnSubmit)
+    const btnSubmit = await screen.findByText("BUSCAR")
 
-        expect(spy).toHaveBeenNthCalledWith(2,{
-            name:"camisa polo",
-            category:"Todas",
-            maxPrice:"",
-            minPrice:"",
-            orderBy:"desc"
-        })
+    const inputName = await screen.findByPlaceholderText(
+    "FAÇA UMA BUSCA"
+    )
+
+    fireEvent.change(minPrice, {
+    target: {
+        value: "50"
+    }
+    })
+
+    fireEvent.change(maxPrice, {
+    target: {
+        value: "100"
+    }
+    })
+
+    fireEvent.change(inputName, {
+    target: {
+        value: "camisa polo"
+    }
+    })
+
+    await waitFor(() => {
+    expect(minPrice).toHaveValue(50)
+    expect(maxPrice).toHaveValue(100)
+    })
+
+    fireEvent.click(btnSubmit)
+
+    await waitFor(() => {
+    expect(spy).toHaveBeenNthCalledWith(2, {
+        name: "camisa polo",
+        category: "Todas",
+        maxPrice: "",
+        minPrice: "",
+        orderBy: "desc"
+    })
+    })
     })
 })
