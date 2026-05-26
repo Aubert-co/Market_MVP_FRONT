f75f22e36e90d079f4c6638a57f844c93dd08751
aubert-co <blacket021@gmail.com>
2026-05-25 23:40:08 -0300

    feat(store-products-filter): add stock filter params to URL hook

--- app/src/hooks/store/storeProduct/useUrlParams.tsx
+++ app/src/hooks/store/storeProduct/useUrlParams.tsx
@@ -1,18 +1,20 @@
 import { useSearchParams } from "react-router-dom"
 import { checkIsAValidNumber, containsValues } from "@/utils/checkIsValid"
 import { createUrlUpdater } from "@/utils"
-import type { Category, CategoryOption, ProductSortOption } from "@/types/filters.types"
+import type { Category, CategoryOption, OrderBy } from "@/types/filters.types"
 import type { OrderStatus } from "@/types/storeDashboard.types"
-import { allCategoriesOption, PRODUCT_SORT_ARRAY } from "@/constants/filters"
+import { allCategoriesOption, SORT_OPTIONS } from "@/constants/filters"
 
 export const useUrlParams = ()=>{
     const [searchParams,setSearchParams] = useSearchParams()
     
     const searchQuery = searchParams.get("q") ?? undefined;
+    const rawStockOrderBy = searchParams.get("stock_order")
+    const stockOrderBy = containsValues<OrderBy>(rawStockOrderBy,SORT_OPTIONS) ? rawStockOrderBy : "asc"
     const rawCategory = searchParams.get("categoria") 
     const urlCategory = containsValues<CategoryOption>(rawCategory,allCategoriesOption) ? rawCategory : "Todas"
     const rawOrderby = searchParams.get('orderby')
-    const urlOrderby = containsValues<ProductSortOption>(rawOrderby,PRODUCT_SORT_ARRAY) ? rawOrderby : "price_asc" 
+    const urlOrderby = containsValues<OrderBy>(rawOrderby,SORT_OPTIONS) ? rawOrderby : "asc" 
     const rawPage = searchParams.get('page') ?? undefined
     const urlPage = checkIsAValidNumber(rawPage) ? Number(rawPage) : undefined
     const updateUrlParams = createUrlUpdater(setSearchParams)
@@ -23,15 +25,18 @@ export const useUrlParams = ()=>{
     const changeUrlCategory = (category:Category)=>{
         updateUrlParams('categoria',category)
     }
-    const changeUrlOrderby = (orderby:ProductSortOption)=>{
-        updateUrlParams('orderby',orderby)
+    const changeUrlOrderby = (orderby:OrderBy)=>{
+        updateUrlParams('price_order',orderby)
     }
     const changeUrlOrderStatus = (orderStatus:OrderStatus)=>{
         updateUrlParams('status',orderStatus)
     }
+    const changeStockOrderBy = (orderBy:OrderBy)=>{
+        updateUrlParams("stock_order",orderBy)
+    }
     return {
-        urlCategory,urlOrderby,searchQuery,
+        urlCategory,urlOrderby,searchQuery,stockOrderBy,
         changePage,changeUrlCategory,changeUrlOrderby,
-        urlPage,changeUrlOrderStatus
+        urlPage,changeUrlOrderStatus,changeStockOrderBy
     }
 }
