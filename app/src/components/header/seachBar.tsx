e2077439c80abcc177c55880c7f0e65481fe4b6d
aubert-co <blacket021@gmail.com>
2026-07-12 17:00:49 -0300

    feat: improve search bar and add clear option

--- app/src/components/header/seachBar.tsx
+++ app/src/components/header/seachBar.tsx
@@ -1,4 +1,5 @@
 import {  useState } from "react"
+import styled from "styled-components"
 
 
 type Props = {
@@ -6,9 +7,24 @@ type Props = {
     initialValue?:string
 }
 
-type SearchEvent = (params:string)=>void
+type SearchEvent = (params:string,redirect?:boolean)=>void
 
+const BtnClean = styled.button`
+ background: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
 
+  font-size: 14px;
+  font-weight: 600;
+  color: #666;
+
+  padding: 0;
+  margin-right: 8px;
+
+  line-height: 1;
+  
+`
 export const SearchBar = ({searchEvent,initialValue}:Props)=>{
     const [searchValue,setSearchValue] = useState<string>( initialValue ?? "")
     const onClick = (e:React.FormEvent<HTMLFormElement>)=>{
@@ -16,8 +32,13 @@ export const SearchBar = ({searchEvent,initialValue}:Props)=>{
       
         if(searchValue.length ===0)return;
 
-        searchEvent( searchValue )
+        searchEvent( searchValue ,true)
         
+    }
+   const clear = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        searchEvent('')
+        setSearchValue('')
     }
     return (
         <form onSubmit={(e)=>onClick(e)} className="search" name="form_search">
@@ -27,6 +48,14 @@ export const SearchBar = ({searchEvent,initialValue}:Props)=>{
                     name="input_search"  minLength={2} maxLength={20}
                     className="input_search"
                     placeholder="FAÇA UMA BUSCA"/>
+                 {searchValue && (
+                    <BtnClean 
+                    onMouseDown={(e) => e.preventDefault()}
+                    className="btn_clear_search"
+                     type="button" onClick={clear}>
+                        X
+                    </BtnClean>
+                    )}
                 <button className="btn_search" name="btn_search" type="submit" >BUSCAR</button>
             </div>
         </form>
