33cdb969ae2adf1ec08d2569976b58333a3cdd94
aubert-co <blacket021@gmail.com>
2026-09-08 19:08:51 -0300

    refactor(checkout): use currency conversion utility

--- app/src/components/checkout/listCheckoutItems.tsx
+++ app/src/components/checkout/listCheckoutItems.tsx
@@ -2,7 +2,7 @@ import type { ItemsCheckout } from "@/types/checkout.types"
 import { CheckoutActions } from "./checkoutActions"
 import type { SetStateAction } from "react";
 import { CheckoutItem } from "@/styles/checkout.style";
-import { loadImage } from "@/utils";
+import { brlCurrency, loadImage } from "@/utils";
 
 
 type PropsList = {
@@ -17,12 +17,12 @@ export const ListCheckoutItems = ({datas,setUpdate}:PropsList)=>{
 
                     <div className="item-details">
                       <p className="item-name">{item.name}</p>
-                      <p className="item-name">Preço:R${item.price}</p>
+                      <p className="item-name">Preço: {brlCurrency(item.price)}</p>
                       <p className="item-name">Estoque:{item.stock}</p>
-                      <p className="item-name">  Total RS:{(item.price * item.quantity).toFixed(2)}</p>
+                      <p className="item-price">  Total {brlCurrency(item.price * item.quantity)}</p>
                     </div>
                 </div>
-
+ 
                 <div className="actions">
                     <CheckoutActions stock={item.stock} setUpdate={setUpdate} id={item.id} quantity={item.quantity} />
                 </div>
