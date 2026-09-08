d5984a8c40c0833eee3e005d9e923cdc20b52ce3
aubert-co <blacket021@gmail.com>
2026-09-08 19:38:51 -0300

    fix(react): add ids to promo array items

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -6,36 +6,43 @@ export const CHECKOUT_KEY = "checkout-key"
 
 export const PROMO_ARRAY = [
   {
+    id:33,
     search: "q=&category=Roupas",
     title: "Roupas",
     content: "Confira peças disponíveis na loja"
   },
   {
+    id:35,
     search: "q=&category=Esporte",
     title: "Esporte",
     content: "Produtos para atividades físicas"
   },
   {
+    id:45,
     search: "q=&category=Livros",
     title: "Livros",
     content: "Explore títulos disponíveis"
   },
   {
+    id:39,
     search: "q=&category=Eletrônicos",
     title: "Eletrônicos",
     content: "Gadgets e acessórios modernos"
   },
   {
+    id:79,
     search: "q=&category=Cozinha",
     title: "Cozinha",
     content: "Utensílios para o dia a dia"
   },
   {
+    id:99,
     search: "q=&category=Informática",
     title: "Informática",
     content: "Equipamentos e tecnologia"
   },
    {
+    id:101,
     search: "q=&maxPrice=50",
     title: "Até R$50",
     content: "Produtos com preço acessível"
